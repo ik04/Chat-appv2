1f16c88c55a892cd36153b6bfccd50192120b603
ik04 <ishaankhurana123@gmail.com>
2024-03-14 17:29:40 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Sentinel
+
+Sentinel is an advanced safety platform designed to protect children in online communication environments. This repository hosts the source code and documentation for our comprehensive safety solution.
+Features
+
+- Real-time Chat Monitoring: Monitors chat conversations in real-time for inappropriate content.
+- Profanity Detection: Utilizes NER (Named Entity Recognition) for identifying personal information and flagging instances of profanity.
+- Speech to Text: Converts audio messages into text for analysis and monitoring.
+- Link Analysis: Scans URLs within chat messages for signs of suspicious or malicious content.
+- Explicit Image Detection: Filters images containing nudity, violence, or graphic imagery.
+- Spam/Fraud Account Detection: Identifies and mitigates deceptive practices such as spamming and fraudulent account creation.
+- Document Analysis: Phrases Documents to detect profanity and explicit content.
---
+++ server/rest/app/Http/Controllers/MessageController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\Message as EnumsMessage;
+use App\Exceptions\EmptyMessageException;
+use App\Exceptions\FlagMessageException;
+use App\Exceptions\RoomNotFoundException;
+use App\Http\Requests\CreateMessageRequest;
+use App\Http\Requests\FetchMessagesRequest;
+use App\Models\Message;
+use App\Models\Room;
+use Exception;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use League\CommonMark\Extension\CommonMark\Node\Inline\Code;
+use Ramsey\Uuid\Uuid;
+
+class MessageController extends Controller
+{
+    public function convertImageToBase64($imagePath){
+        $imageData = file_get_contents($imagePath);
+        $base64Image = base64_encode($imageData);
+        return $base64Image;
+    }
+    
+    public function storeMessage(CreateMessageRequest $request){
+        try{
+            $validated = $request->validated();
+            if(!(isset($validated["message"]) || isset($validated["message_file"]))){
+                throw new EmptyMessageException(message:"Nothing sent in message",code:400);
+            }
+            $room = Room::select("id")->where("uuid",$validated["room_uuid"])->first();
+            if(!$room){
+                throw new RoomNotFoundException(message:"Invalid room uuid, room not found", code:404);
+            }
+            $roomId = $room->id;
+            $client = new Client();
+            if(isset($validated["message_file"])){
+                // save image
+                $image = $request->file('message_file');
+
+                $extension = $image->getClientOriginalExtension();
+                $imageName = 'message_' . time() . '_' . uniqid() . '.' . $extension;
+                Storage::disk('public')->put("/messages/".$imageName, file_get_contents($image));
+                $url = Storage::url("messages/".$imageName);
+                $publicPath = public_path($url);
+                $message = Message::create([
+                    "user_id" => $request->user()->id,
+                    "room_id" => $roomId,
+                    "uuid" => Uuid::uuid4(),
+                    "message" => $url,
+                    "type" => EnumsMessage::IMAGE->value
+                ]);
+                return response()->json(["message" => "Message Stored!","message" => $message]);
+            }
+
+            $message = Message::create([
+                "user_id" => $request->user()->id,
+                "room_id" => $roomId,
+                "uuid" => Uuid::uuid4(),
+                "message" => $validated["message"],
+                "type" => EnumsMessage::TEXT->value
+            ]);
+            return response()->json(["message" => "Message Stored!","message" => $message]);
+        }
+        catch(EmptyMessageException $e){
+            return response()->json(["error" => $e->getMessage()],$e->getCode());
+        }
+        catch(RoomNotFoundException $e){
+            return response()->json(["error" => $e->getMessage()],$e->getCode());
+        }
+        catch(FlagMessageException $e){
+            return response()->json(["error" => $e->getMessage()],$e->getCode());
+        }
+        catch(Exception $e){
+        }
+    }
+
+    public function fetchMessages(FetchMessagesRequest $request){
+        try{
+            $validated = $request->validated();
+            $room = Room::select("id")->where("uuid",$validated["room_uuid"])->first();
+            if(!$room){
+                throw new RoomNotFoundException(message:"Invalid room uuid, room not found", code:404);
+            }
+            $roomId = $room->id;
+            $messages = Message::join("users","messages.user_id","=","users.id")->where("room_id",$roomId)->get(["name","message","messages.uuid","messages.type"]);
+            return response()->json($messages,200);
+        }catch(Exception $e){
+            return response()->json(["error" => $e->getMessage()],$e->getCode());
+        }
+    }
+}
